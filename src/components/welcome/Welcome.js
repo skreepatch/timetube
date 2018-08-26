6454e9947c5c6ee8ac38bd7c907f09060d88463d
Yuri Friedberg <yuri@minutemedia.com>
2018-08-26 14:08:23 +0300

    tests

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -17,34 +17,32 @@ export class Welcome extends Component {
         }
     }
 
-    render() {
-        const login = () => {
-            if (!this.props.me.isLoggedIn) {
-                return <div className="LoginButton" onClick={this.fbLogin()}>Login With Facebook</div>
-            }
-        };
+    loginButton() {
+        if (!this.props.me.isLoggedIn) {
+            return <div className="LoginButton" onClick={this.fbLogin()}>Login With Facebook</div>
+        }
+    }
 
-        const insights = () => {
-            if(this.props.me.id && this.props.timetube && this.props.timetube.videos) {
-                const untilDate = new moment(this.props.timetube.discoveredUntil).format('ll');
-                const videos = Object.keys(this.props.timetube.videos).length;
-                const label = videos === 1 ? 'video' : 'videos';
-                const amount = () => (<div>You have shared <b>{videos} {label}</b> directly on your timeline!</div>);
-                const dicoveredUntil = () => (<div>You have discovered your timetube since <b>{untilDate}</b></div>);
-                return <div className="Insights">
-                    <h3>Welcome back {this.props.me.name}</h3>
-                    {dicoveredUntil()}
-                    {amount()}
-                </div>
-            }
-        };
-
-        return (
-            <div className="Welcome">
-                <h2>Discover all them YouTube videos you have ever shared</h2>
-                {login()}
-                {insights()}
+    insightsPane() {
+        if(this.props.me.id && this.props.timetube && this.props.timetube.videos) {
+            const untilDate = new moment(this.props.timetube.discoveredUntil).format('ll');
+            const videos = Object.keys(this.props.timetube.videos).length;
+            const label = videos === 1 ? 'video' : 'videos';
+            const amount = () => (<div>You have shared <b>{videos} {label}</b> directly on your timeline!</div>);
+            const dicoveredUntil = () => (<div>You have discovered your timetube since <b>{untilDate}</b></div>);
+            return <div className="Insights">
+                <h3>Welcome back {this.props.me.name}</h3>
+                {dicoveredUntil()}
+                {amount()}
             </div>
-        );
+        }
+    }
+
+    render() {
+        return <div className="Welcome">
+                <h2 className="Slogan">Discover all them YouTube videos you have ever shared</h2>
+                {this.loginButton()}
+                {this.insightsPane()}
+            </div>
     }
 }
--- src/components/welcome/Welcome.test.js
+++ src/components/welcome/Welcome.test.js
@@ -2,25 +2,13 @@ import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import Enzyme, { shallow } from 'enzyme';
 import { Welcome } from './Welcome';
+import { store } from "../../store";
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Welcome />', () => {
     it('renders the welcome heading', () => {
-        const me = {
-            name: 'Welcome Component',
-            isLoggedIn: true
-        };
-
-        const timetube = {
-            videos: {},
-            paging: {},
-            fetching: false,
-            discoveredUntil: null,
-            drained: false
-        };
-
-        const wrapper = shallow(<Welcome me={me} timetube={timetube} />);
-        expect(wrapper.find('h2')).toBeDefined();
+        const wrapper = shallow(<Welcome store={store} />);
+        expect(wrapper.find('h2').exists()).toBe(true);
     });
 });
